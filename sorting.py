569ec9712e85aff0e18cab034fa0e6aa5cdc423d
Jaechul Jung <johnnynv6@gmail.com>
2022-09-03 10:35:23 -0500

    DrawInformation Class

---
+++ sorting.py
@@ -0,0 +1,32 @@
+import pygame
+import random
+pygame.init()
+
+
+class DrawInformation:
+    BLACK = 0, 0, 0
+    WHITE = 255, 255, 255
+    GREEN = 0, 255, 0
+    RED = 255, 0, 0
+    GRAY = 128, 128, 128
+    BACKGROUND_COLOR = WHITE
+
+    SIDE_PAD = 100
+    TOP_PAD = 150
+
+    def __init__(self, width, height, lst):
+        self.width = width
+        self.height = height
+
+        self.window = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Sorting Algorithm Visualization")
+        self.set_list(lst)
+
+    def set_list(self, lst):
+        self.lst = lst
+        self.min_val = min(lst)
+        self.max_val = max(lst)
+
+        self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
+        self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+        self.start_x = self.SIDE_PAD // 2
